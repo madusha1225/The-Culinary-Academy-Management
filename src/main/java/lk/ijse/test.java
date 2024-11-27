e8934e31b28cb330e93409835dd64d3fe6dceae7
madusha1225 <madushapullaperuma15@gmail.com>
2024-11-27 18:40:38 +0530

    Files add

---
+++ src/main/java/lk/ijse/test.java
@@ -0,0 +1,9 @@
+package lk.ijse;
+
+import lk.ijse.util.PasswordStorage;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println(PasswordStorage.hashPassword("1234"));
+    }
+}
